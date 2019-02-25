d8cdaa631e31a090fd7356448bc5e99a6c95faff
sobird <i@sobird.me>
2019-02-25 20:53:58 +0800

    add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+/**
+ * Description
+ * 
+ * @author  Yang,junlong at 2019-02-25 20:46:28 build.
+ * @version $Id$
+ */
+
+module.exports = {
+  publicPath: '',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  pwa: {},
+
+  pluginOptions: {}
+}
