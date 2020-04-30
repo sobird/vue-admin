753a4a9eb49b3b6a76b6d96aed355427dc194eb1
sobird <i@sobird.me>
2020-04-30 10:47:43 +0800

    config update

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,21 @@
 /**
  * Description
- * 
+ *
  * @author  Yang,junlong at 2019-02-25 20:46:28 build.
  * @version $Id$
  */
 
 module.exports = {
-  publicPath: '',
-  outputDir: 'dist',
-  assetsDir: 'static',
+  publicPath: "",
+  outputDir: "dist",
+  assetsDir: "static",
   pwa: {},
   pluginOptions: {},
 
   devServer: {
     port: 8080,
+    open: true,
+    hot: true
   },
+  lintOnSave: false
 };
