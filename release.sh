8f9a6160ef1708ffc3d5e6196236c53c97022819
sobird <i@sobird.me>
2020-06-03 12:17:02 +0800

    add release.sh

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+SHA=$(git rev-parse HEAD) 
+THIS_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+VERSION=$(npm ls ~ | grep -o "[0-9]*\.[0-9]*\.[0-9]*")
+
+DIST=dist
+REMOTE=origin
+BRANCH=gh-pages
+BIN=node_modules/.bin
+
+npm run build
+
+git checkout ${BRANCH}
+cp -r ${DIST}/* ./
+
+if (( $THIS_BRANCH == master ))
+then
+    git add .
+    git commit -n -am "Auto build from ${SHA}"
+    git push ${REMOTE} ${BRANCH}
+    git checkout ${THIS_BRANCH}
+fi
