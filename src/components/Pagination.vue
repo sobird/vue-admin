8ccae3dd685069c74afd01be0cb5d63e3f00a7fb
sobird <i@sobird.me>
2020-09-27 00:02:05 +0800

    分页组件参数规范优化

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,6 +1,10 @@
 <!--
- - Pagination.vue
- - 
+ - 列表分页设置
+ - pt: page total 总条目数
+ - ps: page size 每页显示条目个数
+ - pc: page count 页码总数
+ - pn: page number 当前页数
+ -
  - sobird<i@sobird.me> at 2020/03/03 14:44:49 created.
 -->
 
@@ -8,80 +12,87 @@
   <el-pagination
     background
     :hide-on-single-page="true"
-    @current-change="onCurrentChange"
-    @size-change="onSizeChange"
-    :current-page="currentPage"
-    :page-sizes="pageSizes"
-    :page-size="pageSize"
-    :total="total"
+    @current-change="pageChange"
+    @size-change="sizeChange"
+    :current-page="pn"
+    :page-sizes="pss"
+    :page-size="ps"
+    :total="pt"
     layout=" prev, pager, next, jumper"
   ></el-pagination>
 </template>
 
 <script>
 import Cookies from 'js-cookie';
+
 export default {
   data() {
     return {
-      currentPage: 1,
-      pageSize: 20
+      pn: 1,
+      ps: 20,
     };
   },
   props: {
-    total: {
+    // page total
+    pt: {
       type: [String, Number],
-      default: 0
+      default: 0,
     },
-    pageSizes: {
+
+    // page sizes
+    pss: {
       type: Array,
-      default: () => [20, 30, 50, 100]
+      default: () => [20, 30, 50, 100],
     },
+
+    // page number change
     change: {
       type: Function,
-      default: function() {}
-    }
+      default: () => {},
+    },
   },
   created() {
     const { query } = this.$route;
-    this.currentPage = Number(query.page);
-    this.pageSize =
-      Number(query.limit) ||
-      Number(Cookies.get("pageSize")) ||
-      this.pageSize;
+    this.pn = Number(query.page);
+    this.ps = Number(query.limit) || Number(Cookies.get('ps')) || this.ps;
 
-    query.limit = this.pageSize;
+    query.limit = this.ps;
   },
   methods: {
-    onCurrentChange(currentPage) {
+    // 页码变化
+    pageChange(pn) {
       this.$router.push({
-        query: Object.assign({}, this.$route.query, { page: currentPage })
+        query: Object.assign({}, this.$route.query, { pn }),
       });
     },
-    onSizeChange(pageSize) {
+    // 页数变化
+    sizeChange(ps) {
       this.$router.push({
-        query: Object.assign({}, this.$route.query, { limit: pageSize })
+        query: Object.assign({}, this.$route.query, { ps }),
       });
 
-      Cookies.set("pageSize", pageSize);
-    }
+      Cookies.set('ps', ps);
+    },
   },
+
   watch: {
-    "$route.query.limit": function(limit, oldValue) {
-      const query = Object.assign({}, this.$route.query, {limit});
+    '$route.query.pn': function(pn, o) {
+      this.pn = Number(pn || 1);
+      const query = Object.assign({}, this.$route.query, { pn });
 
       this.change(query);
     },
-    "$route.query.page": function(page, oldValue) {
-      this.currentPage = Number(page || 1);
-      const query = Object.assign({}, this.$route.query, {page});
+
+    '$route.query.ps': function(ps, o) {
+      const query = Object.assign({}, this.$route.query, { ps });
 
       this.change(query);
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .el-pagination {
   text-align: center;
   margin-top: 20px;
