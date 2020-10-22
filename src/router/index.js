a00fdc694aee4c2650acfb634519a0a273f53577
sobird <i@sobird.me>
2020-10-22 15:19:16 +0800

    add 404 error page

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,45 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue';
+import Router from 'vue-router';
 
 Vue.use(Router);
 
-import EntryWithAside from "@/components/Layout/EntryWithAside";
-import NavMenuRouter from "./NavMenuRouter";
+import EntryWithAside from '@/components/Layout/EntryWithAside';
+import NavMenuRouter from './NavMenuRouter';
 
 const router = new Router({
   test: [],
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: EntryWithAside,
       children: [
         {
-          path: "",
-          component: () => import("@/views/Home.vue")
-        }
-      ]
+          path: '',
+          component: () => import('@/views/Home.vue'),
+        },
+      ],
     },
-    ...NavMenuRouter
+    ...NavMenuRouter,
+
+    // 404错误页
+    {
+      path: '*',
+      meta: {
+        //title: 'Not Found'
+      },
+      component: EntryWithAside,
+      children: [
+        {
+          path: '',
+          name: 'NotFound',
+          component: () => import('@/views/404.vue'),
+          meta: {
+            title: '页面未找到',
+          },
+        },
+      ],
+    },
   ],
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
@@ -28,7 +47,7 @@ const router = new Router({
     } else {
       return { x: 0, y: 0 };
     }
-  }
+  },
 });
 
 router.beforeEach((to, from, next) => {
@@ -37,11 +56,11 @@ router.beforeEach((to, from, next) => {
 
 const originalPush = Router.prototype.push;
 Router.prototype.push = function push(location) {
-  return originalPush.call(this, location).catch(err => err)
-}
+  return originalPush.call(this, location).catch(err => err);
+};
 
 // 方便操作router query
-Vue.prototype.$query = function (query) {
+Vue.prototype.$query = function(query) {
   this.$router.push({ query: Object.assign({}, this.$route.query, query) });
   return this.$route.query;
 };
---
+++ src/views/404.vue
@@ -0,0 +1,27 @@
+<!--
+ - 404页面未找到错误页
+ - 
+ - sobird<i@sobird.me> at 2020/02/26 21:42:12 created.
+-->
+
+<template>
+  <div class="not-found">
+    <img :src="require('@/assets/not-found.png')" class="error-img" />
+    <h1>抱歉，页面暂时无法访问...</h1>
+    <p>请核对您输入的页面地址是否正确</p>
+    <el-button type="primary" @click="$router.push('/')">返回首页</el-button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.not-found {
+  text-align: center;
+  .error-img {
+    height: 300px;
+    margin-top: 50px;
+  }
+  .el-button {
+    margin-top: 30px;
+  }
+}
+</style>
