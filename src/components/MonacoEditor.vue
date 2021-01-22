29fdab47e54ac65bbfc2fc98844ef63914961236
sobird <i@sobird.me>
2021-01-22 15:08:01 +0800

    升级MonacoEditor组件

--- src/components/MonacoEditor.vue
+++ src/components/MonacoEditor.vue
@@ -23,7 +23,7 @@ export default {
       type: Object,
       default: () => ({}),
     },
-    value: {
+    modelValue: {
       type: String,
     }
   },
@@ -34,7 +34,8 @@ export default {
 
   methods: {
     async initMonacoEditor() {
-      const { value: content } = this;
+      const { modelValue: content } = this;
+
       const defaultOptions = {
         language: 'javascript',
         autoIndent: true,
@@ -57,16 +58,13 @@ export default {
       options.value = content;
 
       await this.$nextTick();
-
       let monacoEditor = monaco.editor.create(this.$refs.monaco, options);
-
       monacoEditor.onDidChangeModelContent(event => {
         let value = monacoEditor.getValue();
 
-        this.$emit('input', value);
+        this.$emit('update:modelValue', value);
         this.$emit('change', value);
       });
-
       this.monacoEditor = monacoEditor;
     },
   },
